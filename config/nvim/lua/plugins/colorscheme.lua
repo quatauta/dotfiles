ea414e55abf3df71377d0de69001230d34330923
Daniel Schömer <daniel.schoemer@gmx.net>
2024-06-06 22:10:20 +0200

    vim: Add colorscheme PaperColor to LazyVim config

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "NLKNguyen/papercolor-theme" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "PaperColor",
+    },
+  },
+}
