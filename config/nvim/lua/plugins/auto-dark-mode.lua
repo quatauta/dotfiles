8afc15fbaea8cf13aff87c25991915f39b51d1a3
Daniel Schömer <daniel.schoemer@gmx.net>
2024-06-08 08:17:19 +0200

    vim: Remove obsolete comments

--- config/nvim/lua/plugins/auto-dark-mode.lua
+++ config/nvim/lua/plugins/auto-dark-mode.lua
@@ -9,11 +9,9 @@ return {
     update_interval = 1000,
     set_dark_mode = function()
       vim.api.nvim_set_option_value("background", "dark", { scope = "global" })
-      -- vim.cmd("colorscheme gruvbox") -- the dark mode colorscheme
     end,
     set_light_mode = function()
       vim.api.nvim_set_option_value("background", "light", { scope = "global" })
-      -- vim.cmd("colorscheme gruvbox") -- the light mode colorscheme
     end,
   },
 }
