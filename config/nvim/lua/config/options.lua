b0bded29dea65307067a266f5de1df458b8f0925
Daniel Schömer <daniel.schoemer@gmx.net>
2024-06-21 22:16:30 +0200

    neovim: Do not auto-format and do not enter minit pairs

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.autoformat = false
+vim.g.minipairs_disable = true
+vim.o.incsearch = true
+vim.o.smarttab = true
+vim.o.softtabstop = 2
