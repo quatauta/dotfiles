e9a9478d588313f2caf6935359a18fa5dc6a5f10
Daniel Schömer <quatauta@users.noreply.github.com>
2024-07-05 16:29:16 +0200

    feat: omz mcfly: Do not set HISTFILE

--- oh-my-zsh/custom/plugins/mcfly/mcfly.plugin.zsh
+++ oh-my-zsh/custom/plugins/mcfly/mcfly.plugin.zsh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Initialize [mcfly](https://github.com/cantino/mcfly/)
 
-export HISTFILE="${XDG_CACHE_HOME:-${HOME}/.cache}/shell-history"
-
 if command -v mcfly 1>/dev/null 2>&1 ; then
     eval "$(mcfly init zsh)"
 fi
