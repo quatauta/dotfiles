91edb386392042584f82a3eec344ec6926da3a84
Daniel Schömer <daniel.schoemer@gmx.net>
2025-07-06 09:18:37 +0200

    nvim: Configure blink-cmp to be less automatic/aggressive and show only the menu, no ghost text

---
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'saghen/blink.cmp', opts = {
+      completion = {
+        ghost_text = {
+          enabled = false,
+          show_with_menu = false,
+        },
+        menu = {
+          auto_show = true,
+        },
+        list = {
+          selection = {
+            auto_insert = false,
+            preselect = false,
+          }
+        }
+      }
+    }
+  }
+}
